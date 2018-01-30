3b9a180a88f40bf2643069c149334b9bc7b3be19
James <jimmysarge@googlemail.com>
2018-01-30 20:08:23 +0000

    choose binary thresholding

--- binarize.py
+++ binarize.py
@@ -5,19 +5,11 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('/home/james/Documents/Practice/sudoku/img/3.jpg', 0)
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
 
-titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+# Choose binary
 
-for i in range(6):
-    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
+image_str = 3
 
-plt.show()
+img = cv2.imread('/home/james/Documents/Practice/sudoku/img/' + str(image_str) + '.jpg', 0)
+
+ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
