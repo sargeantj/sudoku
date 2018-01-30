0fb7a141cfb7ab55813c54a5f5015f2286cfdfc8
James <jimmysarge@googlemail.com>
2018-01-30 20:11:32 +0000

    binarisation threshold level

--- binarize.py
+++ binarize.py
@@ -12,4 +12,10 @@ image_str = 3
 
 img = cv2.imread('/home/james/Documents/Practice/sudoku/img/' + str(image_str) + '.jpg', 0)
 
-ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+ret, thresh = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+
+plt.figure()
+plt.imshow(thresh, 'gray')
+
+plt.figure()
+plt.imshow(img, 'gray')
