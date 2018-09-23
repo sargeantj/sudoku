60240c1969425ca38d60bfabf14df6569067045e
James Sargeant <jimmysarge@googlemail.com>
2018-09-23 12:04:04 +0100

    model improvements

--- extract_square.py
+++ extract_square.py
@@ -79,7 +79,7 @@ def extract_sudoku(corners, image):
 
 def extract_square(image_number=1):
     """Given a number of the image file return a cropped sudoku."""
-    image_string = '/home/james/Documents/Code/sudoku/python-sudoku/img/'
+    image_string = '/home/james/Documents/projects/python-sudoku/img/'
     image_string += str(image_number) + '.jpg'
 
     binary = read_binary(image_string)
@@ -121,7 +121,7 @@ def image_to_csv(image_number):
         result.append(picture_row)
 
     picture_frame = pd.DataFrame(result)
-    picture_frame.to_csv("/home/james/Documents/Code/sudoku/"
+    picture_frame.to_csv("/home/james/Documents/projects/"
                          + 'python-sudoku/training/image_' + str(image_number)
                          + '.csv',
                          header=False, index=False)
@@ -133,7 +133,7 @@ def csv_to_image(image_number):
 
     This is a reverse of the function image_to_csv
     """
-    image = pd.read_csv("/home/james/Documents/Code/sudoku/"
+    image = pd.read_csv("/home/james/Documents/projects/"
                         + 'python-sudoku/training/image_' + str(image_number)
                         + '.csv', header=None)
     result = []
--- model.py
+++ model.py
@@ -15,7 +15,7 @@ def scale_image(images):
             for value in row:
                 line.append(np.float32(value/255))
             current.append(line)
-        result.append(np.array(current))
+        result.append(np.array(current).reshape(33, 33, 1))
     return result
 
 
@@ -31,14 +31,45 @@ def one_hot_y(list_y):
 
 def get_model():
     """Return the model used to predict a number."""
+    # Sequential Model
     model = kr.models.Sequential()
-    model.add(kr.layers.Conv2D(32, input_shape=(1, 33, 33), kernel_size=(5, 5),
-                               activation='relu', padding='same'))
+
+    # Layers
+    model.add(kr.layers.Conv2D(32, input_shape=(33, 33, 1),
+                               kernel_size=(5, 5), activation='relu',
+                               padding='same'))
     model.add(kr.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2),
                                   padding='same'))
     model.add(kr.layers.Conv2D(64, kernel_size=(5, 5), activation='relu',
                                padding='same'))
     model.add(kr.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2),
                                   padding='same'))
-    model.add()
+    model.add(kr.layers.Dense(10, activation='relu'))
+
+    # Compile
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+
+    # Return the model
     return model
+
+
+
+import extract_square as es
+data = es.csv_to_image(1)
+
+y = [8, 0, 0, 4, 0, 0, 6, 0, 1, 2, 0, 0, 6, 0, 5, 8, 4, 0, 0, 0, 0, 0, 0,
+     0, 0, 5, 0, 0, 0, 6, 5, 4, 0, 0, 0, 8, 5, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+     0, 1, 8, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 9, 0, 6, 0, 0, 1, 0, 8,
+     4, 7, 0, 3, 0, 0, 9, 0, 0, 1, 0, 5]
+
+oney = one_hot_y(y)
+
+x = scale_image(data)
+
+q = np.array(x)
+
+my_y = np.array(np.array(oney))
+
+model = get_model()
+
+model.fit(x=q, y=my_y, epochs=10, batch_size=81)
