25c13e341311c6cd5724111bd1e926308f9e5a5a
James Sargeant <jimmysarge@googlemail.com>
2019-03-24 17:26:16 +0000

    table display the results

--- sudoku/app.py
+++ sudoku/app.py
@@ -37,6 +37,21 @@ def upload_image():
     return render_template('result.html', result=result)
 
 
+@app.route('/example')
+def example():
+    """Display an example result."""
+    result = [[8, 7, 5, 4, 9, 2, 6, 3, 1],
+              [2, 9, 3, 6, 1, 5, 8, 4, 7],
+              [1, 6, 4, 7, 8, 3, 2, 5, 9],
+              [9, 3, 6, 5, 4, 1, 7, 2, 8],
+              [5, 8, 2, 3, 7, 6, 9, 1, 4],
+              [7, 4, 1, 8, 2, 9, 5, 6, 3],
+              [4, 1, 8, 2, 5, 7, 3, 9, 6],
+              [6, 5, 9, 1, 3, 8, 4, 7, 2],
+              [3, 2, 7, 9, 6, 4, 1, 8, 5]]
+    return render_template('result.html', result=result)
+
+
 def process_image(image):
     """Given an image process it."""
     # Tensorflow
