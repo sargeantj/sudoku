0ff2f7be244538a38e4660c857f20896d6a8f9b5
James Sargeant <jimmysarge@googlemail.com>
2019-03-24 13:29:49 +0000

    refacoring

--- request.py
+++ request.py
@@ -3,9 +3,20 @@
 import requests
 from extract_square import csv_to_image
 
-if __name__ == '__main__':
+
+def numeric_image():
+    """Post a numeric image."""
     image = csv_to_image(1)
     r = requests.post('http://localhost:5000/numeric_image',
                       data={'numeric_image': image})
 
     print(r)
+
+
+def raw_image():
+    """Make a request with an image."""
+    img = cv2.imread(input_string)
+    r = requests.post('http://localhost:5000', data={'image': img})
+
+
+if __name__ == '__main__':
--- sudoku/app.py
+++ sudoku/app.py
@@ -1,8 +1,12 @@
 """Web application main file."""
 
 from flask import Flask, render_template, request
+import os
+
+UPLOAD_FOLDER = '/upload'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.getcwd() + UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -15,5 +19,9 @@ def home():
 def upload_image():
     """Upload image route."""
     file = request.files['image']
-
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
     return render_template('index.html')
+
+
+def process_image():
+    """Given an image process it."""
